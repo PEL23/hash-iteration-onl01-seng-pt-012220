208f4088c32e750d56d3129d154682c44ab72285
excellent-validator-4322 <plarmony24@gmail.com>
2020-02-02 20:13:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,7 +9,7 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
    
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  if #{age} > 12 
+  if age > 12 
   puts "You are too old for this."
     
   end
